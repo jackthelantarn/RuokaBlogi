766a22abe63182708870ed2dcb2506f4b7360488
jackthelantern <naimanowrin123@gmail.com>
2021-12-07 21:07:24 +0200

    map marker added

--- js/map.js
+++ js/map.js
@@ -1,30 +1,39 @@
 "use strict"
 
 // get current location
-function getlocation(){ 
+function getlocation() {
     let lat, long;
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(position =>{
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
             lat = position.coords.latitude
             long = position.coords.longitude
-            setmap ([position.coords.longitude,position.coords.latitude])
-       },error =>{
-           let error_text =document.getElementById('error_text')
-           error_text.innerHTML="Enable location to see result"
-       })
+            setmap([position.coords.longitude, position.coords.latitude])
+        }, error => {
+            let error_text = document.getElementById('error_text')
+            error_text.innerHTML = "Enable location to see result"
+        })
     }
-   
+
 }
 
-// create a map object
-function setmap(center){
+// create new map and location marker
+function setmap(center) {
     mapboxgl.accessToken = 'pk.eyJ1IjoibmFpbWFuIiwiYSI6ImNrd3dkNjZ6eDAybW8yb3A4a3llamgydWoifQ.nXkTuK4XZ_pcxbi11UK3pQ';
+    
     const map = new mapboxgl.Map({
-    container: 'map', 
-    style: 'mapbox://styles/mapbox/streets-v11', 
-    center: center, 
-    zoom: 14 
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: center,
+        zoom: 14
     });
+
+    const marker = new mapboxgl.Marker({
+        color: "red",
+        draggable: true
+        }).setLngLat(center)
+        .addTo(map);
 }
+
+
 
 getlocation()
