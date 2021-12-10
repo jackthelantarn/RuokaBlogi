802621665dcc2abb6622c85633ab3d9516e56b28
jackthelantern <naimanowrin123@gmail.com>
2021-12-10 13:29:29 +0200

    get restaurant.json

--- js/map.js
+++ js/map.js
@@ -5,8 +5,6 @@ function getlocation() {
     let lat, long;
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
-            lat = position.coords.latitude
-            long = position.coords.longitude
             setmap([position.coords.longitude, position.coords.latitude]);
             getdata([position.coords.longitude, position.coords.latitude]);
         }, error => {
@@ -20,7 +18,7 @@ function getlocation() {
 // create new map and location marker
 function setmap(center) {
     mapboxgl.accessToken = 'pk.eyJ1IjoibmFpbWFuIiwiYSI6ImNrd3dkNjZ6eDAybW8yb3A4a3llamgydWoifQ.nXkTuK4XZ_pcxbi11UK3pQ';
-
+    // create map object
     const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
@@ -35,22 +33,22 @@ function setmap(center) {
         .addTo(map);
 }
 
-
+//
 function getdata(center) {
     fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/restaurant.json?type=poi&limit=20&proximity=" + center[0] + "," + center[1] + "&access_token=pk.eyJ1IjoibmFpbWFuIiwiYSI6ImNrd3dkNjZ6eDAybW8yb3A4a3llamgydWoifQ.nXkTuK4XZ_pcxbi11UK3pQ")
-    .then(function (response){
-        return response.json
-    }).then(function (json) {
-        console.log(json)
-        //restaurantMarker(json)
-    }).catch (function(error) {
-        console.log(error)
-    })
-    
+        .then(function (response) {
+            return response.json()
+        }).then(function (json) {
+            restaurantMarker(json)
+        }).catch(function (error) {
+            console.log(error)
+        })
+
 
 }
 
-function restaurantMarker(json_data){
+
+function restaurantMarker(json_data) {
     console.log(json_data)
 }
 
